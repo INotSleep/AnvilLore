8f37f58c304f58d602563294e0babab628f1758f
INotSleep <77581131+INotSleep@users.noreply.github.com>
2023-08-22 15:48:28 +0300

    v1.0

---
+++ src/main/java/me/inotsleep/anvillore/AnvilLore.java
@@ -0,0 +1,17 @@
+package me.inotsleep.anvillore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AnvilLore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/inotsleep/anvillore/Listeners.java
@@ -0,0 +1,2 @@
+package me.inotsleep.anvillore;public class Listeners {
+}
